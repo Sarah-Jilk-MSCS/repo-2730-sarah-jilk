3a64999d795e30ba4d09e909dd565d1fa775fe92
sjjil <sjjil@DESKTOP-PUMIKNF>
2016-09-06 20:35:50 -0500

    Adding 1F1

---
+++ sjilk_1f1/Main Form.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblProjSales = New System.Windows.Forms.Label()
+        Me.lblCurrentSales = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtStraw = New System.Windows.Forms.TextBox()
+        Me.txtBlue = New System.Windows.Forms.TextBox()
+        Me.txtRasp = New System.Windows.Forms.TextBox()
+        Me.lblStraw = New System.Windows.Forms.Label()
+        Me.lblBlue = New System.Windows.Forms.Label()
+        Me.lblRasp = New System.Windows.Forms.Label()
+        Me.txtProjIncrease = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(29, 384)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 29)
+        Me.btnCalc.TabIndex = 7
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(148, 384)
+        Me.btnPrint.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 29)
+        Me.btnPrint.TabIndex = 8
+        Me.btnPrint.Text = "&Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(271, 384)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 29)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(393, 384)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 29)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblProjSales
+        '
+        Me.lblProjSales.AutoSize = True
+        Me.lblProjSales.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProjSales.Location = New System.Drawing.Point(220, 41)
+        Me.lblProjSales.Name = "lblProjSales"
+        Me.lblProjSales.Size = New System.Drawing.Size(195, 56)
+        Me.lblProjSales.TabIndex = 11
+        Me.lblProjSales.Text = "Projected &increate % " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(in decimal form):"
+        '
+        'lblCurrentSales
+        '
+        Me.lblCurrentSales.AutoSize = True
+        Me.lblCurrentSales.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCurrentSales.Location = New System.Drawing.Point(218, 160)
+        Me.lblCurrentSales.Name = "lblCurrentSales"
+        Me.lblCurrentSales.Size = New System.Drawing.Size(111, 23)
+        Me.lblCurrentSales.TabIndex = 12
+        Me.lblCurrentSales.Text = "Current Sales"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(374, 160)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 23)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Projected Sales"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(74, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 23)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "&Strawberries:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(74, 263)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 23)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "&Blueberries:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(74, 323)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 23)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "&Raspberries:"
+        '
+        'txtStraw
+        '
+        Me.txtStraw.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStraw.Location = New System.Drawing.Point(214, 202)
+        Me.txtStraw.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtStraw.Name = "txtStraw"
+        Me.txtStraw.Size = New System.Drawing.Size(100, 27)
+        Me.txtStraw.TabIndex = 2
+        '
+        'txtBlue
+        '
+        Me.txtBlue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlue.Location = New System.Drawing.Point(213, 256)
+        Me.txtBlue.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtBlue.Name = "txtBlue"
+        Me.txtBlue.Size = New System.Drawing.Size(100, 27)
+        Me.txtBlue.TabIndex = 4
+        '
+        'txtRasp
+        '
+        Me.txtRasp.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRasp.Location = New System.Drawing.Point(213, 316)
+        Me.txtRasp.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtRasp.Name = "txtRasp"
+        Me.txtRasp.Size = New System.Drawing.Size(100, 27)
+        Me.txtRasp.TabIndex = 6
+        '
+        'lblStraw
+        '
+        Me.lblStraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblStraw.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStraw.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblStraw.Location = New System.Drawing.Point(384, 202)
+        Me.lblStraw.Name = "lblStraw"
+        Me.lblStraw.Size = New System.Drawing.Size(100, 27)
+        Me.lblStraw.TabIndex = 14
+        Me.lblStraw.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblBlue
+        '
+        Me.lblBlue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBlue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlue.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblBlue.Location = New System.Drawing.Point(384, 256)
+        Me.lblBlue.Name = "lblBlue"
+        Me.lblBlue.Size = New System.Drawing.Size(100, 27)
+        Me.lblBlue.TabIndex = 15
+        Me.lblBlue.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblRasp
+        '
+        Me.lblRasp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRasp.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRasp.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblRasp.Location = New System.Drawing.Point(384, 316)
+        Me.lblRasp.Name = "lblRasp"
+        Me.lblRasp.Size = New System.Drawing.Size(100, 27)
+        Me.lblRasp.TabIndex = 16
+        Me.lblRasp.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtProjIncrease
+        '
+        Me.txtProjIncrease.Location = New System.Drawing.Point(411, 61)
+        Me.txtProjIncrease.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtProjIncrease.Name = "txtProjIncrease"
+        Me.txtProjIncrease.Size = New System.Drawing.Size(45, 27)
+        Me.txtProjIncrease.TabIndex = 0
+        Me.txtProjIncrease.Text = "0.05"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.sjilk_1f1.My.Resources.Resources.berries
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 24)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(177, 138)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 17
+        Me.PictureBox1.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 438)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtProjIncrease)
+        Me.Controls.Add(Me.lblRasp)
+        Me.Controls.Add(Me.lblBlue)
+        Me.Controls.Add(Me.lblStraw)
+        Me.Controls.Add(Me.txtRasp)
+        Me.Controls.Add(Me.txtBlue)
+        Me.Controls.Add(Me.txtStraw)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblCurrentSales)
+        Me.Controls.Add(Me.lblProjSales)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cranston Berries"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblProjSales As Label
+    Friend WithEvents lblCurrentSales As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtStraw As TextBox
+    Friend WithEvents txtBlue As TextBox
+    Friend WithEvents txtRasp As TextBox
+    Friend WithEvents lblStraw As Label
+    Friend WithEvents lblBlue As Label
+    Friend WithEvents lblRasp As Label
+    Friend WithEvents txtProjIncrease As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ sjilk_1f1/Main Form.vb
@@ -0,0 +1,29 @@
+﻿' Name:         Cranston Berries Applications   
+' Purpose:      Calculates projected increase in sales
+' Programmer:   Sarah Jilk on 09/06/2016
+
+Public Class frmMain
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        lblBlue.Text = Format(Val(txtBlue.Text) * (1 + Val(txtProjIncrease.Text)), "currency")
+        lblRasp.Text = Format(Val(txtRasp.Text) * (1 + Val(txtProjIncrease.Text)), "currency")
+        lblStraw.Text = Format(Val(txtStraw.Text) * (1 + Val(txtProjIncrease.Text)), "currency")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear values
+        lblBlue.Text = String.Empty
+        lblRasp.Text = String.Empty
+        lblStraw.Text = String.Empty
+        txtBlue.Text = String.Empty
+        txtRasp.Text = String.Empty
+        txtStraw.Text = String.Empty
+        ' set focus to projected increase box
+        txtProjIncrease.Focus()
+        ' set projected increase back to 0.05
+        txtProjIncrease.Text = "0.05"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
