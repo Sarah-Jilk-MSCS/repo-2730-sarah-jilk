6aee2ff13aa666340027d484db84212fc41ee19b
sjjil <sjjil@DESKTOP-PUMIKNF>
2016-10-22 10:36:01 -0500

    Add changes for Ex2G1

---
+++ SJILK_2G1/Form1.Designer.vb
@@ -0,0 +1,416 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.lblDiscount = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.btnCalcIF = New System.Windows.Forms.Button()
+        Me.btnCalcNIF = New System.Windows.Forms.Button()
+        Me.btnCalcIfElseIF = New System.Windows.Forms.Button()
+        Me.btnCalcSelect = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(77, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(149, 129)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Discount Rates"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(84, 90)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(28, 17)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "3%"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(84, 73)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 17)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "2.5%"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(84, 56)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(28, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "2%"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(84, 39)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "0%"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(40, 90)
+        Me.Label4.Name = "Label4"
+        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label4.Size = New System.Drawing.Size(36, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "20+:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label3.Size = New System.Drawing.Size(49, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "16-20:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label2.Size = New System.Drawing.Size(49, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "11-15:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label1.Size = New System.Drawing.Size(41, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "1-10:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.Label15)
+        Me.GroupBox2.Controls.Add(Me.Label16)
+        Me.GroupBox2.Location = New System.Drawing.Point(264, 27)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(149, 129)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Example"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(114, 90)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(24, 17)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "$9"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(114, 73)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(24, 17)
+        Me.Label10.TabIndex = 6
+        Me.Label10.Text = "$5"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(114, 56)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(24, 17)
+        Me.Label11.TabIndex = 5
+        Me.Label11.Text = "$3"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(114, 39)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(24, 17)
+        Me.Label12.TabIndex = 4
+        Me.Label12.Text = "$0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(27, 90)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(86, 17)
+        Me.Label13.TabIndex = 3
+        Me.Label13.Text = "$10, Qty 30:"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(27, 73)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(86, 17)
+        Me.Label14.TabIndex = 2
+        Me.Label14.Text = "$10, Qty 20:"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(27, 56)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(86, 17)
+        Me.Label15.TabIndex = 1
+        Me.Label15.Text = "$10, Qty 15:"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(27, 39)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(86, 17)
+        Me.Label16.TabIndex = 0
+        Me.Label16.Text = "$10, Qty 10:"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(34, 218)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(44, 17)
+        Me.Label17.TabIndex = 2
+        Me.Label17.Text = "&Price:"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(136, 218)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(65, 17)
+        Me.Label18.TabIndex = 3
+        Me.Label18.Text = "&Quantity:"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(261, 218)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(67, 17)
+        Me.Label19.TabIndex = 4
+        Me.Label19.Text = "Discount:"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(378, 218)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(74, 17)
+        Me.Label20.TabIndex = 5
+        Me.Label20.Text = "Total Due:"
+        '
+        'lblDiscount
+        '
+        Me.lblDiscount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDiscount.Location = New System.Drawing.Point(248, 238)
+        Me.lblDiscount.Name = "lblDiscount"
+        Me.lblDiscount.Size = New System.Drawing.Size(100, 22)
+        Me.lblDiscount.TabIndex = 6
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Location = New System.Drawing.Point(365, 238)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(100, 22)
+        Me.lblTotalDue.TabIndex = 7
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(12, 238)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 22)
+        Me.txtPrice.TabIndex = 8
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(126, 238)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(100, 22)
+        Me.txtQuantity.TabIndex = 9
+        '
+        'btnCalcIF
+        '
+        Me.btnCalcIF.Location = New System.Drawing.Point(12, 309)
+        Me.btnCalcIF.Name = "btnCalcIF"
+        Me.btnCalcIF.Size = New System.Drawing.Size(100, 32)
+        Me.btnCalcIF.TabIndex = 10
+        Me.btnCalcIF.Text = "Calc:If"
+        Me.btnCalcIF.UseVisualStyleBackColor = True
+        '
+        'btnCalcNIF
+        '
+        Me.btnCalcNIF.Location = New System.Drawing.Point(118, 309)
+        Me.btnCalcNIF.Name = "btnCalcNIF"
+        Me.btnCalcNIF.Size = New System.Drawing.Size(116, 32)
+        Me.btnCalcNIF.TabIndex = 11
+        Me.btnCalcNIF.Text = "Calc: NestedIf"
+        Me.btnCalcNIF.UseVisualStyleBackColor = True
+        '
+        'btnCalcIfElseIF
+        '
+        Me.btnCalcIfElseIF.Location = New System.Drawing.Point(248, 309)
+        Me.btnCalcIfElseIF.Name = "btnCalcIfElseIF"
+        Me.btnCalcIfElseIF.Size = New System.Drawing.Size(111, 32)
+        Me.btnCalcIfElseIF.TabIndex = 12
+        Me.btnCalcIfElseIF.Text = "Calc: If-ElseIf"
+        Me.btnCalcIfElseIF.UseVisualStyleBackColor = True
+        '
+        'btnCalcSelect
+        '
+        Me.btnCalcSelect.Location = New System.Drawing.Point(365, 309)
+        Me.btnCalcSelect.Name = "btnCalcSelect"
+        Me.btnCalcSelect.Size = New System.Drawing.Size(100, 32)
+        Me.btnCalcSelect.TabIndex = 13
+        Me.btnCalcSelect.Text = "Calc: Select"
+        Me.btnCalcSelect.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(196, 347)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 32)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 393)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalcSelect)
+        Me.Controls.Add(Me.btnCalcIfElseIF)
+        Me.Controls.Add(Me.btnCalcNIF)
+        Me.Controls.Add(Me.btnCalcIF)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.lblDiscount)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SJILK_2G1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents lblDiscount As Label
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents btnCalcIF As Button
+    Friend WithEvents btnCalcNIF As Button
+    Friend WithEvents btnCalcIfElseIF As Button
+    Friend WithEvents btnCalcSelect As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ SJILK_2G1/Form1.vb
@@ -0,0 +1,183 @@
+﻿' Name:         Jetters Project
+' Purpose:      Display the discount and total due
+' Programmer:   Sarah Jilk on 10/22/2016
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtPrice_Enter(sender As Object, e As EventArgs) Handles txtPrice.Enter
+        txtPrice.SelectAll()
+    End Sub
+
+    Private Sub txtQuantity_Enter(sender As Object, e As EventArgs) Handles txtQuantity.Enter
+        txtQuantity.SelectAll()
+    End Sub
+
+    Private Sub ClearLabel(sender As Object, e As EventArgs) Handles txtPrice.TextChanged, txtQuantity.TextChanged
+        lblDiscount.Text = String.Empty
+        lblTotalDue.Text = String.Empty
+    End Sub
+
+    Private Sub btnCalcIF_Click(sender As Object, e As EventArgs) Handles btnCalcIF.Click
+        ' calculate discount and total due
+
+        Dim dblPrice As Double
+        Dim intQuantity As Integer
+        Dim dblRate As Double
+        Dim dblTotalWithoutDiscount As Double
+        Dim dblDiscount As Double
+        Dim dblTotalDue As Double
+
+        Double.TryParse(txtPrice.Text, dblPrice)
+        Integer.TryParse(txtQuantity.Text, intQuantity)
+
+        If intQuantity > 0 AndAlso intQuantity <= 10 Then
+            dblRate = 0.0
+        End If
+
+        If intQuantity > 10 AndAlso intQuantity <= 15 Then
+            dblRate = 0.02
+        End If
+
+        If intQuantity > 15 AndAlso intQuantity <= 20 Then
+            dblRate = 0.025
+        End If
+
+        If intQuantity > 20 Then
+            dblRate = 0.03
+        End If
+
+        dblTotalWithoutDiscount = dblPrice * intQuantity
+        dblDiscount = dblTotalWithoutDiscount * dblRate
+        dblTotalDue = dblTotalWithoutDiscount - dblDiscount
+
+        lblDiscount.Text = dblDiscount.ToString("C2")
+        lblTotalDue.Text = dblTotalDue.ToString("C2")
+    End Sub
+
+    Private Sub txtQuantity_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtQuantity.KeyPress
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> ControlChars.Back Then
+            ' cancel the key
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtPrice_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPrice.KeyPress
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> ControlChars.Back AndAlso
+                e.KeyChar <> "." Then
+            ' cancel the key
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub btnCalcNIF_Click(sender As Object, e As EventArgs) Handles btnCalcNIF.Click
+        ' calculate discount and total due
+
+        Dim dblPrice As Double
+        Dim intQuantity As Integer
+        Dim dblRate As Double
+        Dim dblTotalWithoutDiscount As Double
+        Dim dblDiscount As Double
+        Dim dblTotalDue As Double
+
+        Double.TryParse(txtPrice.Text, dblPrice)
+        Integer.TryParse(txtQuantity.Text, intQuantity)
+
+        If intQuantity > 0 AndAlso intQuantity <= 10 Then
+            dblRate = 0.0
+        Else
+            If intQuantity <= 15 Then
+                dblRate = 0.02
+            Else
+                If intQuantity <= 20 Then
+                    dblRate = 0.025
+                Else
+                    dblRate = 0.03
+                End If
+            End If
+        End If
+
+        dblTotalWithoutDiscount = dblPrice * intQuantity
+        dblDiscount = dblTotalWithoutDiscount * dblRate
+        dblTotalDue = dblTotalWithoutDiscount - dblDiscount
+
+        lblDiscount.Text = dblDiscount.ToString("C2")
+        lblTotalDue.Text = dblTotalDue.ToString("C2")
+    End Sub
+
+    Private Sub btnCalcIfElseIF_Click(sender As Object, e As EventArgs) Handles btnCalcIfElseIF.Click
+        ' calculate discount and total due
+
+        Dim dblPrice As Double
+        Dim intQuantity As Integer
+        Dim dblRate As Double
+        Dim dblTotalWithoutDiscount As Double
+        Dim dblDiscount As Double
+        Dim dblTotalDue As Double
+
+        Double.TryParse(txtPrice.Text, dblPrice)
+        Integer.TryParse(txtQuantity.Text, intQuantity)
+
+        If intQuantity > 0 AndAlso intQuantity <= 10 Then
+            dblRate = 0.0
+        ElseIf intQuantity <= 15 Then
+            dblRate = 0.02
+        ElseIf intQuantity <= 20 Then
+            dblRate = 0.025
+        Else dblRate = 0.03
+        End If
+
+        dblTotalWithoutDiscount = dblPrice * intQuantity
+        dblDiscount = dblTotalWithoutDiscount * dblRate
+        dblTotalDue = dblTotalWithoutDiscount - dblDiscount
+
+        lblDiscount.Text = dblDiscount.ToString("C2")
+        lblTotalDue.Text = dblTotalDue.ToString("C2")
+
+    End Sub
+
+    Private Sub btnCalcSelect_Click(sender As Object, e As EventArgs) Handles btnCalcSelect.Click
+        ' calculate discount and total due
+
+        Dim dblPrice As Double
+        Dim intQuantity As Integer
+        Dim dblRate As Double
+        Dim dblTotalWithoutDiscount As Double
+        Dim dblDiscount As Double
+        Dim dblTotalDue As Double
+
+        Double.TryParse(txtPrice.Text, dblPrice)
+        Integer.TryParse(txtQuantity.Text, intQuantity)
+
+        Select Case intQuantity
+            Case 1 To 10
+                dblRate = 0.0
+            Case 11 To 15
+                dblRate = 0.02
+            Case 16 To 20
+                dblRate = 0.025
+            Case > 20
+                dblRate = 0.03
+            Case Else
+                dblRate = 0.0
+        End Select
+
+        dblTotalWithoutDiscount = dblPrice * intQuantity
+        dblDiscount = dblTotalWithoutDiscount * dblRate
+        dblTotalDue = dblTotalWithoutDiscount - dblDiscount
+
+        lblDiscount.Text = dblDiscount.ToString("C2")
+        lblTotalDue.Text = dblTotalDue.ToString("C2")
+    End Sub
+End Class
+
