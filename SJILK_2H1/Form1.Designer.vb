0cb53a9c88aa916d96904ea1f4e9a9bf68cc1f46
sjjil <sjjil@DESKTOP-PUMIKNF>
2016-10-29 09:30:35 -0500

    Changes for SJILK_2H1

---
+++ SJILK_2H1/Form1.Designer.vb
@@ -0,0 +1,396 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblCommission = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblAdditional = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.chkTraveling = New System.Windows.Forms.CheckBox()
+        Me.chkOver10 = New System.Windows.Forms.CheckBox()
+        Me.btnCalcIf = New System.Windows.Forms.Button()
+        Me.btbCalcNestIf = New System.Windows.Forms.Button()
+        Me.btnCalcIfElse = New System.Windows.Forms.Button()
+        Me.btnCalcSelect = New System.Windows.Forms.Button()
+        Me.txtSales = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Sales:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Commission only:"
+        '
+        'lblCommission
+        '
+        Me.lblCommission.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCommission.Location = New System.Drawing.Point(42, 117)
+        Me.lblCommission.Name = "lblCommission"
+        Me.lblCommission.Size = New System.Drawing.Size(100, 23)
+        Me.lblCommission.TabIndex = 10
+        Me.lblCommission.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(39, 169)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(126, 17)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Additional Amount:"
+        '
+        'lblAdditional
+        '
+        Me.lblAdditional.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdditional.Location = New System.Drawing.Point(42, 186)
+        Me.lblAdditional.Name = "lblAdditional"
+        Me.lblAdditional.Size = New System.Drawing.Size(100, 23)
+        Me.lblAdditional.TabIndex = 14
+        Me.lblAdditional.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(189, 123)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(153, 17)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Total due salesperson:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.Khaki
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(192, 145)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(136, 51)
+        Me.lblTotal.TabIndex = 12
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'chkTraveling
+        '
+        Me.chkTraveling.AutoSize = True
+        Me.chkTraveling.Location = New System.Drawing.Point(192, 64)
+        Me.chkTraveling.Name = "chkTraveling"
+        Me.chkTraveling.Size = New System.Drawing.Size(89, 21)
+        Me.chkTraveling.TabIndex = 3
+        Me.chkTraveling.Text = "&Traveling"
+        Me.chkTraveling.UseVisualStyleBackColor = True
+        '
+        'chkOver10
+        '
+        Me.chkOver10.AutoSize = True
+        Me.chkOver10.Location = New System.Drawing.Point(192, 37)
+        Me.chkOver10.Name = "chkOver10"
+        Me.chkOver10.Size = New System.Drawing.Size(120, 21)
+        Me.chkOver10.TabIndex = 2
+        Me.chkOver10.Text = "&Over 10 years"
+        Me.chkOver10.UseVisualStyleBackColor = True
+        '
+        'btnCalcIf
+        '
+        Me.btnCalcIf.Location = New System.Drawing.Point(379, 28)
+        Me.btnCalcIf.Name = "btnCalcIf"
+        Me.btnCalcIf.Size = New System.Drawing.Size(105, 30)
+        Me.btnCalcIf.TabIndex = 4
+        Me.btnCalcIf.Text = "Calc If"
+        Me.btnCalcIf.UseVisualStyleBackColor = True
+        '
+        'btbCalcNestIf
+        '
+        Me.btbCalcNestIf.Location = New System.Drawing.Point(379, 64)
+        Me.btbCalcNestIf.Name = "btbCalcNestIf"
+        Me.btbCalcNestIf.Size = New System.Drawing.Size(105, 30)
+        Me.btbCalcNestIf.TabIndex = 5
+        Me.btbCalcNestIf.Text = "Calc NestIf"
+        Me.btbCalcNestIf.UseVisualStyleBackColor = True
+        '
+        'btnCalcIfElse
+        '
+        Me.btnCalcIfElse.Location = New System.Drawing.Point(379, 100)
+        Me.btnCalcIfElse.Name = "btnCalcIfElse"
+        Me.btnCalcIfElse.Size = New System.Drawing.Size(105, 30)
+        Me.btnCalcIfElse.TabIndex = 6
+        Me.btnCalcIfElse.Text = "Calc IfElse"
+        Me.btnCalcIfElse.UseVisualStyleBackColor = True
+        '
+        'btnCalcSelect
+        '
+        Me.btnCalcSelect.Location = New System.Drawing.Point(379, 139)
+        Me.btnCalcSelect.Name = "btnCalcSelect"
+        Me.btnCalcSelect.Size = New System.Drawing.Size(105, 30)
+        Me.btnCalcSelect.TabIndex = 7
+        Me.btnCalcSelect.Text = "Calc Select"
+        Me.btnCalcSelect.UseVisualStyleBackColor = True
+        '
+        'txtSales
+        '
+        Me.txtSales.Location = New System.Drawing.Point(42, 50)
+        Me.txtSales.Name = "txtSales"
+        Me.txtSales.Size = New System.Drawing.Size(100, 22)
+        Me.txtSales.TabIndex = 1
+        Me.txtSales.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(379, 179)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(105, 30)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label17)
+        Me.GroupBox1.Controls.Add(Me.Label16)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label15)
+        Me.GroupBox1.Controls.Add(Me.Label14)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Label11)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 235)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(445, 142)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Commission Rates"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(147, 70)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(271, 17)
+        Me.Label15.TabIndex = 7
+        Me.Label15.Text = "$3,120 plus 14% of the sales over 30,000"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(147, 52)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(251, 17)
+        Me.Label14.TabIndex = 6
+        Me.Label14.Text = "$120 plus 13% of the sales over 6,000"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(147, 35)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(89, 17)
+        Me.Label13.TabIndex = 5
+        Me.Label13.Text = "10% of sales"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(147, 18)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(83, 17)
+        Me.Label12.TabIndex = 4
+        Me.Label12.Text = "Commission"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(6, 70)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(84, 17)
+        Me.Label11.TabIndex = 3
+        Me.Label11.Text = "30,000.00 +"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(6, 52)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(113, 17)
+        Me.Label10.TabIndex = 2
+        Me.Label10.Text = "6,000-29,999.99"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(6, 35)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(77, 17)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "1-5,999.99"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(6, 18)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(65, 17)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Sales ($)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(79, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(133, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Additional Amounts:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(79, 104)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = ">10 Years"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(79, 121)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(67, 17)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Traveling"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(167, 104)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(60, 17)
+        Me.Label16.TabIndex = 11
+        Me.Label16.Text = "$500.00"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(167, 121)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(60, 17)
+        Me.Label17.TabIndex = 12
+        Me.Label17.Text = "$700.00"
+        '
+        'frm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(499, 385)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.txtSales)
+        Me.Controls.Add(Me.btnCalcSelect)
+        Me.Controls.Add(Me.btnCalcIfElse)
+        Me.Controls.Add(Me.btbCalcNestIf)
+        Me.Controls.Add(Me.btnCalcIf)
+        Me.Controls.Add(Me.chkOver10)
+        Me.Controls.Add(Me.chkTraveling)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblAdditional)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblCommission)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frm1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SJILK_2H1_Marshall"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblCommission As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblAdditional As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents chkTraveling As CheckBox
+    Friend WithEvents chkOver10 As CheckBox
+    Friend WithEvents btnCalcIf As Button
+    Friend WithEvents btbCalcNestIf As Button
+    Friend WithEvents btnCalcIfElse As Button
+    Friend WithEvents btnCalcSelect As Button
+    Friend WithEvents txtSales As TextBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ SJILK_2H1/Form1.vb
@@ -0,0 +1,201 @@
+﻿Public Class frm1
+    Private Sub btnCalcIf_Click(sender As Object, e As EventArgs) Handles btnCalcIf.Click
+        lblCommission.Text = String.Empty
+        lblAdditional.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        Dim dblSales As Double = 0.0
+        Dim dblCommission As Double = 0.0
+        Dim dblAdditional As Double = 0.0
+        Dim dblTotal As Double = 0.0
+        Dim boolSalesOK As Boolean = Double.TryParse(txtSales.Text, dblSales)
+
+        If Not boolSalesOK Then
+            MessageBox.Show("Invalid input", "Number Format Error", MessageBoxButtons.OK)
+        End If
+
+        If boolSalesOK AndAlso dblSales < 1.0 Then
+            dblCommission = 0.0
+        End If
+
+        If boolSalesOK AndAlso dblSales >= 1.0 AndAlso dblSales < 6000.0 Then
+            dblCommission = dblSales * 0.1
+        End If
+
+        If boolSalesOK AndAlso dblSales >= 6000.0 AndAlso
+                dblSales < 30000.0 Then
+            dblCommission = 120.0 + (dblSales - 6000.0) * 0.13
+        End If
+
+        If boolSalesOK AndAlso dblSales >= 30000.0 Then
+            dblCommission = 3120.0 + (dblSales - 30000.0) * 0.14
+        End If
+
+        If chkOver10.Checked Then
+            dblAdditional = dblAdditional + 500.0
+        End If
+
+        If chkTraveling.Checked Then
+            dblAdditional = dblAdditional + 700.0
+        End If
+
+        dblTotal = dblCommission + dblAdditional
+
+        If boolSalesOK Then
+            lblCommission.Text = dblCommission.ToString("c2")
+            lblAdditional.Text = dblAdditional.ToString("c2")
+            lblTotal.Text = dblTotal.ToString("c2")
+        End If
+
+        txtSales.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtSales_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSales.KeyPress
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> ControlChars.Back AndAlso e.KeyChar <> "." Then
+            'cancel the key
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub btbCalcNestIf_Click(sender As Object, e As EventArgs) Handles btbCalcNestIf.Click
+        lblCommission.Text = String.Empty
+        lblAdditional.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        Dim dblSales As Double = 0.0
+        Dim dblCommission As Double = 0.0
+        Dim dblAdditional As Double = 0.0
+        Dim dblTotal As Double = 0.0
+        Dim boolSalesOK As Boolean = Double.TryParse(txtSales.Text, dblSales)
+
+        If Not boolSalesOK Then
+            MessageBox.Show("Invalid input", "Number Format Error", MessageBoxButtons.OK)
+        End If
+
+        If boolSalesOK AndAlso dblSales < 1.0 Then
+            dblCommission = 0.0
+        Else
+            If dblSales >= 1.0 AndAlso dblSales < 6000.0 Then
+                dblCommission = dblSales * 0.1
+            Else
+                If dblSales >= 6000.0 AndAlso
+                    dblSales < 30000.0 Then
+                    dblCommission = 120.0 + (dblSales - 6000.0) * 0.13
+                Else
+                    If dblSales >= 30000.0 Then
+                        dblCommission = 3120.0 + (dblSales - 30000.0) * 0.14
+                    End If
+                End If
+            End If
+        End If
+
+        If chkOver10.Checked Then
+            dblAdditional = dblAdditional + 500.0
+        End If
+
+        If chkTraveling.Checked Then
+            dblAdditional = dblAdditional + 700.0
+        End If
+
+        dblTotal = dblCommission + dblAdditional
+
+        lblCommission.Text = dblCommission.ToString("c2")
+        lblAdditional.Text = dblAdditional.ToString("c2")
+        lblTotal.Text = dblTotal.ToString("c2")
+
+        txtSales.Focus()
+    End Sub
+
+    Private Sub btnCalcIfElse_Click(sender As Object, e As EventArgs) Handles btnCalcIfElse.Click
+        lblCommission.Text = String.Empty
+        lblAdditional.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        Dim dblSales As Double = 0.0
+        Dim dblCommission As Double = 0.0
+        Dim dblAdditional As Double = 0.0
+        Dim dblTotal As Double = 0.0
+        Dim boolSalesOK As Boolean = Double.TryParse(txtSales.Text, dblSales)
+
+        If Not boolSalesOK Then
+            MessageBox.Show("Invalid input", "Number Format Error", MessageBoxButtons.OK)
+        End If
+
+        If boolSalesOK AndAlso dblSales < 1.0 Then
+            dblCommission = 0.0
+        ElseIf dblSales >= 1.0 AndAlso dblSales < 6000.0 Then
+            dblCommission = dblSales * 0.1
+        ElseIf dblSales >= 6000.0 AndAlso dblSales < 30000.0 Then
+            dblCommission = 120.0 + (dblSales - 6000.0) * 0.13
+        ElseIf dblSales >= 30000.0 Then
+            dblCommission = 3120.0 + (dblSales - 30000.0) * 0.14
+        End If
+
+        If chkOver10.Checked Then
+            dblAdditional = dblAdditional + 500.0
+        End If
+
+        If chkTraveling.Checked Then
+            dblAdditional = dblAdditional + 700.0
+        End If
+
+        dblTotal = dblCommission + dblAdditional
+
+        lblCommission.Text = dblCommission.ToString("c2")
+        lblAdditional.Text = dblAdditional.ToString("c2")
+        lblTotal.Text = dblTotal.ToString("c2")
+
+        txtSales.Focus()
+    End Sub
+
+    Private Sub btnCalcSelect_Click(sender As Object, e As EventArgs) Handles btnCalcSelect.Click
+        lblCommission.Text = String.Empty
+        lblAdditional.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        Dim dblSales As Double = 0.0
+        Dim dblCommission As Double = 0.0
+        Dim dblAdditional As Double = 0.0
+        Dim dblTotal As Double = 0.0
+        Dim boolSalesOK As Boolean = Double.TryParse(txtSales.Text, dblSales)
+
+        If Not boolSalesOK Then
+            MessageBox.Show("Invalid input", "Number Format Error", MessageBoxButtons.OK)
+        End If
+
+        Select Case dblSales
+            Case < 1.0
+                dblCommission = 0.0
+            Case 1 To 5999.99
+                dblCommission = dblSales * 0.1
+            Case 6000.0 To 29999.99
+                dblCommission = 120.0 + (dblSales - 6000.0) * 0.13
+            Case >= 30000.0
+                dblCommission = 3120.0 + (dblSales - 30000.0) * 0.14
+        End Select
+
+
+        If chkOver10.Checked Then
+            dblAdditional = dblAdditional + 500.0
+        End If
+
+        If chkTraveling.Checked Then
+            dblAdditional = dblAdditional + 700.0
+        End If
+
+        dblTotal = dblCommission + dblAdditional
+
+        If boolSalesOK Then
+            lblCommission.Text = dblCommission.ToString("c2")
+            lblAdditional.Text = dblAdditional.ToString("c2")
+            lblTotal.Text = dblTotal.ToString("c2")
+        End If
+
+        txtSales.Focus()
+    End Sub
+End Class
