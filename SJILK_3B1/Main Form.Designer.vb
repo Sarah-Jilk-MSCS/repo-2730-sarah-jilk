f41f754e1db709ebd85af1288a56fe43a7e1825c
sjjil <sjjil@DESKTOP-PUMIKNF>
2016-11-29 22:13:54 -0600

    CHanges for 3B1

---
+++ SJILK_3B1/Main Form.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnForNext = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDoLoop = New System.Windows.Forms.Button()
+        Me.lstMultTable = New System.Windows.Forms.ListBox()
+        Me.Page2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblPretax = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lstPrices = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Page2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Number:"
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(93, 25)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(42, 32)
+        Me.txtNumber.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Multiplication table:"
+        '
+        'btnForNext
+        '
+        Me.btnForNext.Location = New System.Drawing.Point(172, 25)
+        Me.btnForNext.Name = "btnForNext"
+        Me.btnForNext.Size = New System.Drawing.Size(111, 33)
+        Me.btnForNext.TabIndex = 2
+        Me.btnForNext.Text = "&For...Next"
+        Me.btnForNext.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(172, 103)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(111, 33)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDoLoop
+        '
+        Me.btnDoLoop.Location = New System.Drawing.Point(172, 64)
+        Me.btnDoLoop.Name = "btnDoLoop"
+        Me.btnDoLoop.Size = New System.Drawing.Size(111, 33)
+        Me.btnDoLoop.TabIndex = 3
+        Me.btnDoLoop.Text = "&Do...Loop"
+        Me.btnDoLoop.UseVisualStyleBackColor = True
+        '
+        'lstMultTable
+        '
+        Me.lstMultTable.FormattingEnabled = True
+        Me.lstMultTable.ItemHeight = 25
+        Me.lstMultTable.Location = New System.Drawing.Point(15, 105)
+        Me.lstMultTable.Name = "lstMultTable"
+        Me.lstMultTable.Size = New System.Drawing.Size(146, 204)
+        Me.lstMultTable.TabIndex = 6
+        '
+        'Page2
+        '
+        Me.Page2.Controls.Add(Me.lstMultTable)
+        Me.Page2.Controls.Add(Me.Label1)
+        Me.Page2.Controls.Add(Me.btnDoLoop)
+        Me.Page2.Controls.Add(Me.txtNumber)
+        Me.Page2.Controls.Add(Me.btnExit)
+        Me.Page2.Controls.Add(Me.Label2)
+        Me.Page2.Controls.Add(Me.btnForNext)
+        Me.Page2.Location = New System.Drawing.Point(12, 12)
+        Me.Page2.Name = "Page2"
+        Me.Page2.Size = New System.Drawing.Size(337, 350)
+        Me.Page2.TabIndex = 7
+        Me.Page2.TabStop = False
+        Me.Page2.Text = "Page 348 #2"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnAdd)
+        Me.GroupBox1.Controls.Add(Me.lblTotal)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.lblTax)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.lblPretax)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.lstPrices)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(385, 16)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(387, 346)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Page #388 #8 with Validation"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(205, 291)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(127, 35)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(202, 224)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(130, 45)
+        Me.lblTotal.TabIndex = 7
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(197, 199)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(61, 25)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Total: "
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTax.Location = New System.Drawing.Point(202, 72)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(130, 45)
+        Me.lblTax.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(197, 117)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 25)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Tax:"
+        '
+        'lblPretax
+        '
+        Me.lblPretax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPretax.Location = New System.Drawing.Point(202, 142)
+        Me.lblPretax.Name = "lblPretax"
+        Me.lblPretax.Size = New System.Drawing.Size(130, 45)
+        Me.lblPretax.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(197, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 25)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Pretax Total: "
+        '
+        'lstPrices
+        '
+        Me.lstPrices.FormattingEnabled = True
+        Me.lstPrices.ItemHeight = 25
+        Me.lstPrices.Location = New System.Drawing.Point(18, 72)
+        Me.lstPrices.Name = "lstPrices"
+        Me.lstPrices.Size = New System.Drawing.Size(125, 254)
+        Me.lstPrices.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 25)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Prices:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 408)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Page2)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Multiplication Table"
+        Me.Page2.ResumeLayout(False)
+        Me.Page2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnForNext As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnDoLoop As Button
+    Friend WithEvents lstMultTable As ListBox
+    Friend WithEvents Page2 As GroupBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblPretax As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lstPrices As ListBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ SJILK_3B1/Main Form.vb
@@ -0,0 +1,95 @@
+﻿' Name:         Multiplication Project
+' Purpose:      Display a multiplication table that shows
+'               the multiplicand, multiplier, and product
+' Programmer:   Sarah Jilk on <current date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Const dblTaxRate As Double = 0.05
+
+    Private Sub btnDoLoop_Click(sender As Object, e As EventArgs) Handles btnDoLoop.Click
+        ' displays a multiplication table
+        Dim number, product, count As Integer
+        lstMultTable.Items.Clear()
+        lstMultTable.Items.Add("Do Loop:")
+        Integer.TryParse(txtNumber.Text, number)
+        'Do Loop
+
+        Do Until count = 9
+            count = count + 1
+            product = number * count
+
+            Dim strOutput As String = "    " & number.ToString() & " * " & count.ToString & " = " & product.ToString()
+            'add strOutput to Listbox
+            lstMultTable.Items.Add(strOutput)
+        Loop
+
+
+
+    End Sub
+
+    Private Sub btnForNext_Click(sender As Object, e As EventArgs) Handles btnForNext.Click
+        ' displays a multiplication table
+        Dim number, product, count As Integer
+        lstMultTable.Items.Clear()
+        lstMultTable.Items.Add("For Loop:")
+        Integer.TryParse(txtNumber.Text, number)
+        'For Loop
+        For count = 1 To 9 Step 1
+            product = number * count
+            Dim strOutput As String = "    " & number.ToString() & " * " & count.ToString & " = " & product.ToString()
+            'add strOutput to Listbox
+            lstMultTable.Items.Add(strOutput)
+        Next count
+
+    End Sub
+
+    Private Sub txtNumber_Enter(sender As Object, e As EventArgs) Handles txtNumber.Enter
+        txtNumber.SelectAll()
+    End Sub
+
+    Private Sub txtNumber_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNumber.KeyPress
+        ' allows only numbers and the Backspace
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtNumber_TextChanged(sender As Object, e As EventArgs) Handles txtNumber.TextChanged
+        lstMultTable.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'load listbox with data
+        For dblPrices As Double = 1 To 4 Step 1
+            lstPrices.Items.Add(dblPrices.ToString("n2"))
+        Next dblPrices
+
+        Dim dblPreTaxTotal As Double
+        Dim strSelectedItem As String
+        Dim index As Integer
+        Dim dblSelectedPrice As Double
+
+        For index = 0 To (lstPrices.Items.Count - 1) Step 1
+            lstPrices.SelectedIndex = index
+            strSelectedItem = lstPrices.ToString()
+            Double.TryParse(strSelectedItem, dblSelectedPrice)
+            dblPreTaxTotal = dblSelectedPrice + dblPreTaxTotal
+
+        Next index
+
+        lblPretax.Text = dblPreTaxTotal.ToString("n2")
+
+
+
+    End Sub
+End Class
